68acaa2362e991a93d7bfc72322f1d1c422bf680
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2016-02-18 16:02:53 +1300

    cleaned up code

--- code/plot_figures.py
+++ code/plot_figures.py
@@ -4,9 +4,9 @@ import seaborn as sns
 import numpy as np
 
 
-def plot_test_noshift1(datadir="../data/"):
+def plot_results(data_file, posterior_file, output_root):
     """
-    Plot results for the data set test_noshift1.dat. 
+    Plot results for a data set. 
     The initial run here had one fixed redshift (which could have been zero), 
     which means that the redshift parameter is in column 9 in the posterior_samples.txt 
     file. Note: This is hard-coded below! If the model changes, especially the 
@@ -16,8 +16,8 @@ def plot_test_noshift1(datadir="../data/"):
 
     ndshift_column = 9 # column with the correct Doppler shift distribution
 
-    data = np.loadtxt(datadir+"test_noshift1.dat")
-    sample = np.atleast_2d(np.loadtxt("test_noshift1_posterior_sample.txt"))
+    data = np.loadtxt(data_file)
+    sample = np.atleast_2d(np.loadtxt(posterior_file))
 
     plt.figure(figsize=(16,6))
     plt.plot(data[:,0], data[:,2], linestyle="steps-mid")
@@ -27,18 +27,31 @@ def plot_test_noshift1(datadir="../data/"):
     plt.xlabel("Wavelength [Angstrom]")
     plt.ylabel("Flux")
     plt.title("First Test: No Doppler shift, all lines same width and amplitude")
-    plt.savefig(datadir+"test_noshift1_samples.png", format="png")
+    plt.savefig(output_root+"_samples.png", format="png")
     plt.close()
 
     plt.figure(figsize=(12,9))
     plt.hist(sample[:,ndshift_column], bins=30, histtype="stepfilled")
     plt.xlabel("Doppler shift $v/c$")
     plt.ylabel("p(Doppler shift)")
-    plt.savefig(datadir+"test_noshift1_dshift.png", format="png")
+    plt.savefig(output_root+"_dshift.png", format="png")
     plt.close()
 
     return
 
 
+def plot_test_noshift1(datadir="../data/"):
+    data_file = datadir+"test_noshift1.dat"
+    posterior_file = datadir+"test_noshift1_posterior_sample.txt"
+    output_root = datadir+"test_noshift1"
+    plot_results(data_file, posterior_file, output_root)
+    return
 
+
+def plot_test_noshift2(datadir="../data/"):
+    data_file = datadir+"test_noshift2.dat"
+    posterior_file = datadir+"test_noshift2_posterior_sample.txt"
+    output_root = datadir+"test_noshift2"
+    plot_results(data_file, posterior_file, output_root)
+    return
 
