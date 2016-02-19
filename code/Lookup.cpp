683463b7e3b6091eb8c1a3a69992aa0fae81637c
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-02-19 16:24:29 +1300

    erf lookup table

---
+++ code/Lookup.cpp
@@ -0,0 +1,34 @@
+#include "Lookup.h"
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+Lookup Lookup::instance(101, -10., 10.);
+
+Lookup::Lookup(size_t num, double x_min, double x_max)
+:num(num)
+,x_min(x_min)
+,x_max(x_max)
+,dx((x_max - x_min)/(num - 1))
+,one_over_dx(1./dx)
+,evaluations(num)
+{
+	assert(x_max > x_min);
+
+	for(size_t i=0; i<num; ++i)
+		evaluations[i] = erf(x_min + i*dx);
+}
+
+double Lookup::erf(double x)
+{
+	if(x < Lookup::x_min)
+		return -1.;
+	if(x > Lookup::x_max)
+		return 1.;
+
+	size_t index = static_cast<size_t>((x - x_min)*one_over_dx);
+	double lambda = (x - (x_min + index*dx))*one_over_dx;
+	return (1. - lambda)*evaluations[index] + lambda*evaluations[index+1];
+}
+
---
+++ code/Lookup.h
@@ -0,0 +1,33 @@
+#ifndef ShiftyLines_Lookup
+#define ShiftyLines_Lookup
+
+#include <vector>
+
+/*
+* Lookup tables for speeding things up
+* Singleton pattern
+*/
+
+class Lookup
+{
+	private:
+		const std::size_t num;
+		const double x_min;
+		const double x_max;
+		const double dx;
+		const double one_over_dx;
+
+		std::vector<double> evaluations;
+
+		Lookup(std::size_t num, double x_min, double x_max);
+		Lookup(const Lookup& other);
+
+		static Lookup instance;
+
+	public:
+		double erf(double x);
+
+};
+
+#endif
+
